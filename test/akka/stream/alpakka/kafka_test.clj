8304431741b4fa0bec8666023b47332b8d34cef5
Mykhailo Hevak <mhevak@gmail.com>
2020-01-16 23:15:14 +0200

    -

--- test/akka/stream/alpakka/kafka_test.clj
+++ test/akka/stream/alpakka/kafka_test.clj
@@ -6,11 +6,14 @@
 (def akka-context (create-context "test"))
 
 (deftest flow-test
-  (let [settings (consumer-settings
-                   (:system akka-context)
-                                    {:bootstrapServers "server1,server2"
-                                     :group-id "group-id"
-                                     :stop-timeout 1000
-                                     :properties ["property1" "value1" "property2" "value2"]})]
+  (let [settings (.getProperties (consumer-settings
+                                   (:system akka-context)
+                                   {:bootstrapServers "server1,server2"
+                                    :group-id         "group-id"
+                                    :stop-timeout     1000
+                                    :properties       ["property1" "value1" "property2" "value2"]}))]
 
-    (println (.getProperties settings))))
+    (is (= "group-id" (.get settings "group.id")))
+    (is (= "server1,server2" (.get settings "bootstrap.servers")))
+    (is (= "value1" (.get settings "property1")))
+    (is (= "value2" (.get settings "property2")))))
