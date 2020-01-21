64d71966cbb435a0246b4cae1db6ce379e3bc2d3
mykhailo.hevak <mykhailo.hevak@appsflyer.com>
2020-01-21 10:48:53 +0200

    -

--- src/clojure/akka/stream/alpakka/kafka.clj
+++ src/clojure/akka/stream/alpakka/kafka.clj
@@ -1,24 +1,39 @@
 (ns akka.stream.alpakka.kafka
   (:require [scala.internal :refer [$]])
-  (:import (akka.kafka ConsumerSettings)
+  (:import (akka.kafka ConsumerSettings Subscription Subscriptions)
            (akka.actor ActorSystem)
            (org.apache.kafka.common.serialization Deserializer StringDeserializer)
-           (java.time Duration)))
+           (java.time Duration)
+           (akka.kafka.scaladsl Consumer)
+           (scala.collection Set)))
 
 (defn consumer-settings
-  ([^ActorSystem actorSystem
-    ^Deserializer keyDeserializer
-    ^Deserializer valueDeserializer
+  ([^ActorSystem actor-system
+    ^Deserializer key-deserializer
+    ^Deserializer value-deserializer
     {bootstrapServers :bootstrapServers
      group-id         :group-id
      stop-timeout     :stop-timeout
      properties       :properties}]
-   (let [settings (-> (ConsumerSettings/apply actorSystem keyDeserializer valueDeserializer)
-                     (.withBootstrapServers bootstrapServers)
-                     (.withGroupId group-id)
-                     (.withStopTimeout (Duration/ofMillis stop-timeout))
-                     )]
+   (let [settings (-> (ConsumerSettings/apply actor-system key-deserializer value-deserializer)
+                      (.withBootstrapServers bootstrapServers)
+                      (.withGroupId group-id)
+                      (.withStopTimeout (Duration/ofMillis stop-timeout))
+                      )]
 
      (reduce #(.withProperty %1 (first %2) (second %2)) settings (partition 2 properties))))
   ([^ActorSystem actorSystem config-map]
    (consumer-settings actorSystem (new StringDeserializer) (new StringDeserializer) config-map)))
+
+(defn- create-subscription [topic]
+  (println topic)
+  ($ Subscriptions/topics & topic))
+
+(defn source-with-offset-context [^ConsumerSettings kafkaConsumerSettings, topic]
+  (Consumer/sourceWithOffsetContext kafkaConsumerSettings (create-subscription topic)))
+
+(defn source-plain [^ConsumerSettings kafkaConsumerSettings, topic]
+  (Consumer/plainSource  kafkaConsumerSettings (create-subscription topic)))
+
+(defn source-committable  [^ConsumerSettings kafkaConsumerSettings, topic]
+  (Consumer/plainSource  kafkaConsumerSettings (create-subscription topic)))
